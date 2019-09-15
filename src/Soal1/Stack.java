eb4ad6a6e438da8fbddc3ee754cdb7120cfe489f
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-15 20:53:46 +0700

    Making attribute in Stack Class

--- src/Soal1/Stack.java
+++ src/Soal1/Stack.java
@@ -5,5 +5,8 @@ package Soal1;
  * @author Etem
  */
 public class Stack {
+    private int max; //Ukuran dari array stack private long[] stackArray;
+    private int top; //Stack Paling atas
+    private long stackArray[] = new long[10];
     
 }
