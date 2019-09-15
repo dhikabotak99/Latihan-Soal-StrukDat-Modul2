498cffd71c39e32dbe4779ccb1f35d6e88d11c81
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-15 21:08:38 +0700

    Make enqueue Method

--- src/Soal2/Queue.java
+++ src/Soal2/Queue.java
@@ -11,4 +11,13 @@ public class Queue {
     private int nItems;//Jumlah Item
     private int[] queue;
     
+    public void enqueue(int e){
+        if (rear==max-1) {//Mengatur lokasi Rear
+            rear=-1;
+        }
+        queue[++rear]=j;//Menabahkan nilai rear dan memasukkan nilai
+        nItems++;//Menambahkan nilai nItems
+    }
 }
+
+    
