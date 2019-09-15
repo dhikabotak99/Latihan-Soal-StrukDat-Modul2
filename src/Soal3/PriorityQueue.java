40434c25c2624f456c0881cbb078eb570a881992
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-15 21:35:16 +0700

    Make Dequeue Method

--- src/Soal3/PriorityQueue.java
+++ src/Soal3/PriorityQueue.java
@@ -8,4 +8,21 @@ public class PriorityQueue {
     private int max;
     private int[] queue;
     private int nItems;
+    
+    public void Enqueue(int e){
+        int j;
+        if (nItems==0) {//Jika tidak ada
+            queue[nItems++]=e;
+        }
+        else{//Jika ada
+            for (j = nItems-1; j >= 0; j--) {//Dimulai dari yang terakhir
+                if (e>queue[j]) {//Jika item baru lebih besar
+                    queue[j+1]=queue[j];//pindahkan ke belakang
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
 }
