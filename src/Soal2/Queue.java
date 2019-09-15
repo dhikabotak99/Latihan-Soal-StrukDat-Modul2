7fa7bcd0fdb52b2680a627533a98179e72dfcf8e
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-15 21:07:26 +0700

    Making Attribute in Queue

--- src/Soal2/Queue.java
+++ src/Soal2/Queue.java
@@ -5,5 +5,10 @@ package Soal2;
  * @author Etem
  */
 public class Queue {
+    private int max;//Membuat nilai maksimal pada array
+    private int front;//Nilai paling awal
+    private int rear;//Nilai paling akhir
+    private int nItems;//Jumlah Item
+    private int[] queue;
     
 }
