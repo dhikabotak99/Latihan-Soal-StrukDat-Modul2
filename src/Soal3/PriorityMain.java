c1561870850043ca05533b26f569970e33612789
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-15 21:47:22 +0700

    Insert data in PriorityMain

--- src/Soal3/PriorityMain.java
+++ src/Soal3/PriorityMain.java
@@ -10,5 +10,17 @@ package Soal3;
  * @author Etem
  */
 public class PriorityMain {
-    
+    public static void main(String[] args){
+        PriorityQueue pq = new PriorityQueue();
+        pq.Enqueue(10);
+        pq.Enqueue(30);
+        pq.Enqueue(50);
+        pq.Dequeue();
+        pq.Enqueue(70);
+        while(!pq.isEmpty()){
+            int item = pq.Dequeue();
+            System.out.print(item+" ");
+        }
+        System.out.println("");
+    }
 }
